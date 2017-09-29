88ac737b9bb60c21d76a0408e490a3f720608f2e
naziml <naziml@gmail.com>
2017-09-28 20:15:36 -0700

    Add SSH capability when hosted on App Service on Linux through Kudu webSSH

---
+++ base-runtime-container/init_container.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+cat >/etc/motd <<EOL 
+  _____                               
+  /  _  \ __________ _________   ____  
+ /  /_\  \\___   /  |  \_  __ \_/ __ \ 
+/    |    \/    /|  |  /|  | \/\  ___/ 
+\____|__  /_____ \____/ |__|    \___  >
+        \/      \/                  \/ 
+          F U N C T I O N S
+
+EOL
+cat /etc/motd
+
+service ssh start
+
+dotnet FunctionsRuntime/Microsoft.Azure.WebJobs.Script.WebHost.dll
